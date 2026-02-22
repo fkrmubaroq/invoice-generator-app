61eeb925da6f1d6fcbf5975ce83e9f2e70d6e1de
fkrmubaroq <fkrmubaroq@gmail.com>
2026-02-22 21:48:59 +0700

    add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,22 +10,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGIN
   ? process.env.ALLOWED_ORIGIN.split(",")
   : [];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow request tanpa origin (postman, curl)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // kalau pakai cookie
-  }),
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
